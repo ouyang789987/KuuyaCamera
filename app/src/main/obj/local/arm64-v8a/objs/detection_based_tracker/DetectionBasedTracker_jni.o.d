aa6691f53c592153dd778ca166d8da56c3069018
kgmyshin <kgmyshin@r.recruit.co.jp>
2015-11-23 03:26:36 +0900

    [add] make kuuya

---
+++ app/src/main/obj/local/arm64-v8a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,149 @@
+/Users/kgmyshin/Work/FaceDetection/app/src/main/obj/local/arm64-v8a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ /Users/kgmyshin/Work/FaceDetection/app/src/main/jni/DetectionBasedTracker_jni.cpp \
+ /Users/kgmyshin/Work/FaceDetection/app/src/main/jni/DetectionBasedTracker_jni.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/jni.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/android/api-level.h \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal/defs.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/limits.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/limits.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/linux/limits.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/syslimits.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cctype \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/sse_utils.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/assert.h \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/deque \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/android/log.h
+
+/Users/kgmyshin/Work/FaceDetection/app/src/main/jni/DetectionBasedTracker_jni.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/jni.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/android/api-level.h:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal/defs.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/limits.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/limits.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/linux/limits.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/syslimits.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/climits:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cctype:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/sse_utils.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/complex:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/assert.h:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/deque:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/android/log.h:
---
+++ app/src/main/obj/local/x86/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,161 @@
+/Users/kgmyshin/Work/FaceDetection/app/src/main/obj/local/x86/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ /Users/kgmyshin/Work/FaceDetection/app/src/main/jni/DetectionBasedTracker_jni.cpp \
+ /Users/kgmyshin/Work/FaceDetection/app/src/main/jni/DetectionBasedTracker_jni.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/jni.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/android/api-level.h \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal/defs.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/limits.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/limits.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/limits.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/machine/internal_types.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/machine/limits.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/syslimits.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/page.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/page_32.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/climits \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cctype \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/string \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/sse_utils.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/assert.h \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+ /Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/deque \
+ /Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/android/log.h
+
+/Users/kgmyshin/Work/FaceDetection/app/src/main/jni/DetectionBasedTracker_jni.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/jni.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/android/api-level.h:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal/defs.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/limits.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/limits.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/limits.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/machine/internal_types.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/machine/limits.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/syslimits.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/page.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/page_32.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/climits:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cctype:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/sse_utils.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/assert.h:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+
+/Users/kgmyshin/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.8/include/deque:
+
+/Users/kgmyshin/Library/Android/sdk/ndk-bundle/platforms/android-9/arch-x86/usr/include/android/log.h:
